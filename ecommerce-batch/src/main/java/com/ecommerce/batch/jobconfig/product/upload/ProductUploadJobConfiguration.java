e27b49c6b737c100a5e2cd92e4f41ce27fdcf903
KJY <kid4211@gmail.com>
2024-11-01 03:24:11 +0900

    CH.05 - 상품 등록 배치 성능 최적화 (파티셔닝 적용 - 속도개선)
    --spring.boot.job.name=productUploadJob inputFilePath=data/random_products.csv,java.lang.String,false gridSize=10,java.lang.Integer,false

--- ecommerce-batch/src/main/java/com/ecommerce/batch/jobconfig/product/upload/ProductUploadJobConfiguration.java
+++ ecommerce-batch/src/main/java/com/ecommerce/batch/jobconfig/product/upload/ProductUploadJobConfiguration.java
@@ -2,14 +2,19 @@ package com.ecommerce.batch.jobconfig.product.upload;
 
 import com.ecommerce.batch.domain.product.Product;
 import com.ecommerce.batch.dto.product.upload.ProductUploadCsvRow;
+import com.ecommerce.batch.service.file.SplitFilePartitioner;
+import com.ecommerce.batch.util.FileUtils;
 import com.ecommerce.batch.util.ReflectionUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepExecutionListener;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.partition.PartitionHandler;
+import org.springframework.batch.core.partition.support.TaskExecutorPartitionHandler;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
@@ -21,6 +26,7 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.support.SynchronizedItemStreamReader;
 import org.springframework.batch.item.support.builder.SynchronizedItemStreamReaderBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +35,7 @@ import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.io.File;
 
 @Slf4j
 @Configuration
@@ -37,12 +44,49 @@ public class ProductUploadJobConfiguration {
     public Job productUploadJob(
             JobRepository jobRepository,
             JobExecutionListener jobExecutionListener,
-            Step productUploadStep
+            Step productUploadPartitionStep
     ) {
         return new JobBuilder("productUploadJob", jobRepository)
                 .listener(jobExecutionListener)
-                .start(productUploadStep)
+                .start(productUploadPartitionStep)
                 .build();
+    }
+
+    @Bean
+    public Step productUploadPartitionStep(
+            PartitionHandler filePartitionHandler,
+            Step productUploadStep,
+            JobRepository jobRepository,
+            SplitFilePartitioner splitFilePartitioner
+    ) {
+        return new StepBuilder("productUploadPartitionStep", jobRepository)
+                .partitioner(productUploadStep.getName(), splitFilePartitioner)
+                .partitionHandler(filePartitionHandler)
+                .allowStartIfComplete(true)
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public SplitFilePartitioner splitFilePartitioner(
+            @Value("#{jobParameters['inputFilePath']}") String path,
+            @Value("#{jobParameters['gridSize']}") int gridSize
+    ) {
+        return new SplitFilePartitioner(FileUtils.splitCsv(new File(path), gridSize));
+    }
+
+    @Bean
+    @JobScope
+    public TaskExecutorPartitionHandler filePartitionHandler(
+            @Qualifier("productUploadExecutor") TaskExecutor taskExecutor,
+            Step productUploadStep,
+            @Value("#{jobParameters['gridSize']}") int gridSize
+    ) {
+        TaskExecutorPartitionHandler handler = new TaskExecutorPartitionHandler();
+        handler.setTaskExecutor(taskExecutor);
+        handler.setStep(productUploadStep);
+        handler.setGridSize(gridSize);
+        return handler;
     }
 
     @Bean
@@ -53,7 +97,7 @@ public class ProductUploadJobConfiguration {
             ItemReader<ProductUploadCsvRow> productReader,
             ItemProcessor<ProductUploadCsvRow, Product> productProcessor,
             ItemWriter<Product> productWriter,
-            TaskExecutor taskExecutor
+            @Qualifier("productUploadExecutor") TaskExecutor taskExecutor
     ) {
         return new StepBuilder("productUploadStep", jobRepository)
                 .<ProductUploadCsvRow, Product>chunk(1000, transactionManager)
@@ -73,16 +117,15 @@ public class ProductUploadJobConfiguration {
     @Bean
     @StepScope
     public SynchronizedItemStreamReader<ProductUploadCsvRow> productReader(
-            @Value("#{jobParameters['inputFilePath']}") String path
+            @Value("#{stepExecutionContext['file']}") File file
     ) {
 
         FlatFileItemReader<ProductUploadCsvRow> fileItemReader = new FlatFileItemReaderBuilder<ProductUploadCsvRow>()
                 .name("productReader")
-                .resource(new FileSystemResource(path))
+                .resource(new FileSystemResource(file))
                 .delimited()
                 .names(ReflectionUtils.getFieldNames(ProductUploadCsvRow.class).toArray(String[]::new))
                 .targetType(ProductUploadCsvRow.class)
-                .linesToSkip(1)
                 .build();
 
         return new SynchronizedItemStreamReaderBuilder<ProductUploadCsvRow>().delegate(fileItemReader).build();
---
+++ ecommerce-batch/src/main/java/com/ecommerce/batch/util/FileUtils.java
@@ -0,0 +1,93 @@
+package com.ecommerce.batch.util;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FileUtils {
+    public static List<File> splitCsv(File csvFile, long fileCount) {
+        return splitFileAfterLineCount(csvFile, fileCount, true, ".csv");
+    }
+
+    public static List<File> splitFileAfterLineCount(File inputFile, long fileCount,
+                                                     boolean ignoreFirstLine, String suffix) {
+        long lineCount;
+        try (Stream<String> stream = Files.lines(inputFile.toPath(), StandardCharsets.UTF_8)) {
+            lineCount = stream.count();
+            return splitFile(inputFile, (long) Math.ceil((double) lineCount / fileCount), ignoreFirstLine,
+                    suffix);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<File> splitFile(File inputFile, long linesPerFile, boolean ignoreFirstLine,
+                                       String suffix)
+            throws IOException {
+        List<File> splitFiles = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+
+        String line;
+        int fileIndex = 0;
+        int lineCount = 0;
+        File splitFile;
+        BufferedWriter writer = null;
+        boolean firstLine = true;
+        boolean shouldCreateFile = true;
+
+        while ((line = reader.readLine()) != null) {
+
+            if (ignoreFirstLine && firstLine) {
+                firstLine = false;
+                continue;
+            }
+            if (shouldCreateFile) {
+                splitFile = createTempFile("split_" + (fileIndex++) + "_", suffix);
+                writer = new BufferedWriter(new FileWriter(splitFile));
+                splitFiles.add(splitFile);
+                lineCount = 0;
+                shouldCreateFile = false;
+            }
+            writer.write(line);
+            writer.newLine();
+            lineCount++;
+
+            if (lineCount >= linesPerFile) {
+                writer.close();
+                shouldCreateFile = true;
+            }
+        }
+
+        writer.close();
+        reader.close();
+
+        return splitFiles;
+    }
+
+    public static File createTempFile(String prefix, String suffix) throws IOException {
+        File tempFile = File.createTempFile(prefix, suffix);
+        tempFile.deleteOnExit();
+        return tempFile;
+    }
+
+
+    public static void mergeFiles(String header, List<File> files, File outputFile) {
+        try (BufferedOutputStream outputStream = new BufferedOutputStream(
+                new FileOutputStream(outputFile))) {
+            outputStream.write((header + "\n").getBytes());
+            for (File partFile : files) {
+                System.out.println("병합 중: " + partFile.getName());
+                Files.copy(partFile.toPath(), outputStream);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<File> splitLog(File logFile, long fileCount) {
+        return splitFileAfterLineCount(logFile, fileCount, false, ".log");
+    }
+}
